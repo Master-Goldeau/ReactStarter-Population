230ba84b628f96e248e064c8b25ba54e3a54898b
Etienne <34507362+Master-Goldeau@users.noreply.github.com>
2018-09-21 18:11:38 +0200

    Add files via upload

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import SelectBar from '../containers/select-bar';
 import SearchBar from '../containers/search-bar';
+const DEFAULT_COUNTRY = "France"
 
 export default class App extends Component {
+
+  onClickSearch(searchCountry){
+    //Requête AJAX à faire cf Section2 - Session 23
+    console.log('---')
+    console.log('',searchCountry)
+    console.log('---')
+
+  }
+
   render() {
     return (
       <div>
-      <SearchBar/>
+        <SelectBar defaultCountry={DEFAULT_COUNTRY} />
+        <SearchBar defaultCountry={DEFAULT_COUNTRY} callback={(event)=>this.onClickSearch(event)}/>
       </div>
     );
   }
