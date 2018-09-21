fd0fa85d61bddeeb3ca7e13245fdd5268e922687
Etienne <34507362+Master-Goldeau@users.noreply.github.com>
2018-09-21 18:11:55 +0200

    Add files via upload

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -4,48 +4,72 @@ import { bindActionCreators } from "redux";
 import { getCountries } from "../actions/index";
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { selectedCountry: this.props.defaultCountry, searchCountry: "", placeHolder: "Search a Country" }
+  }
   componentWillMount() {
     this.props.getCountries();
   }
 
-  renderSelectBox(){
-
-    const {countries} = this.props;
-    if(countries){
-        return (
-            <select className="col-md-12 input-group">
-                {
-                    countries.map((country)=>{
-                        return <option key={country} value={country}>{country}</option>
-                    })
-                }
-            </select>
-        )
-    }else{
-        return <div>No Country found</div>
+  renderSearchBar() {
+    const { countries } = this.props;
+    if (countries) {
+      return (
+        <div>
+          <input
+            // value={this.state.selectedCountry}
+            onChange={(event) => this.handleChange(event)}
+            placeholder={this.state.placeHolder}
+            className="col-lg-12 input-group"
+          >
+          </input>
+          <span className="input-group-btn">
+            <button className="btn btn-secondary" onClick={(event)=> this.handleOnClick(event)}>Search
+            </button>
+          </span>
+        </div>
+      );
+    } else {
+      return <div>No Country found</div>;
     }
+  }
 
+  handleOnClick(event){
+   this.props.callback(this.state.searchCountry)
+
+  }
+
+  handleChange(event) {
+    console.log('---')
+    console.log('-UNE SAISIE-', event.target.value)
+    console.log('---')
+    this.setState({ searchCountry: event.target.value })
+  }
+  
+
+  search(e) {
+    this.setState({ selectedCountry: e.target.value })
+
+    //todo
   }
 
   render() {
-    return <div>
-        {this.renderSelectBox}
-    </div>
-
+    return (
+      <div className="searchBar">
+        {this.renderSearchBar()}
+      </div>
+    )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    countries: state.countries
-  };
+const mapStateToProps = (state) => {
+  return { countries: state.countries };
 };
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ getCountries }, dispatch);
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
